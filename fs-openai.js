f518c5a17400c294b0fc23c91f9263c29bc12366
Felipe Gómez <luisfelipe_1002@hotmail.com>
2025-05-19 16:39:26 -0500

    Script para Transcripción de Audio con OpenAI Whisper y Node.js

---
+++ fs-openai.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+
+async function transcribeAudio(audioFilePath, apiKey) {
+    try {
+        if(!fs.existsSync(audioFilePath)) {
+            throw new Error('El archivo de audio no existe');
+        }
+
+        const audioFile = fs.readFileSync(audioFilePath);
+        const formData = new FormData();
+        const blob = new Blob(audioFile);
+
+        formData.append('file', blob, path.basename(audioFilePath));
+        formData.append('model', 'wwhisper-1');
+
+        const response = await fetch('https://api.openai.com/v1/audio/transciptions', 
+               {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${apiKey}`
+          },
+          body: formData
+        });
+
+        if(!response.ok) {
+            const errorData = await response.json();
+            throw new Error(`Error en la API: ${JSON.stringify(errorData)}`);
+        }
+
+        const data = await response.json();
+        const transciption = data.text;
+
+        const outputFilePath = path.join(path.dirname(audioFile), `${path.basename
+        (audioFilePath, path.extname(audioFilePath))}_transcription.txt`);
+
+        fs.writeFileSync(outputFilePath, transciption);
+        console.log(`Transcripción guardada en: ${outputFilePath}`);
+        return transciption
+    } catch (error) {
+      console.error('Error durante la transcripción', error.message);
+      throw error;
+    }
+}
