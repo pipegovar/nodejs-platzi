59ded60e7c5b5fd8e0ae499a228f1f538cea5bde
Felipe Gómez <luisfelipe_1002@hotmail.com>
2025-05-30 18:14:53 -0500

    Servidor de Video en Node.js: Streams y Buffers

---
+++ server-video.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/video') {
+        const videoPath = path.join(__dirname, 'video.mp4');
+        const stat = fs.statSync(videoPath);
+
+        res.writeHead(200, {
+            'Content-Type': 'video/mp4',
+            'content-lenght': stat.size
+        });
+
+        const readStream = fs.createReadStream(videoPath); // buffer
+        let chunkCounter = 0;
+
+        readStream.on('data', (chunk) => {
+            chunkCounter++;
+            console.log(
+                `Chunk ${chunkCounter}: size: ${chunk.length} bytes, leídos y enviados`
+            );
+        });
+        readStream.pipe(res);
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
+
+server.listen(3005, () => {
+    console.log('Server is Running on http://localhost:3000');   
+})
